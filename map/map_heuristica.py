dde5acaab0f5e5126bf80b49a6306de066fa247b
palcojulian <palcojulian@outlook.com>
2026-05-14 22:50:36 -0500

    docs: :memo: Agrega documentacion codigo

--- map/map_heuristica.py
+++ map/map_heuristica.py
@@ -1,11 +1,8 @@
-# laberinto = [
-#     [1, 0, 1, 1, 1],
-#     [1, 1, 1, 0, 1],
-#     [1, 0, 1, 0, 1],
-#     [1, 0, 1, 1, 1],
-#     [1, 1, 1, 0, 1],
-# ]
-
+"""
+Funcion encargada de calcular la heuristica de manhattan, de acuerdo -
+a la posiciones de la matriz
+d = |x1 - x2| + |y1 - y2|
+"""
 
 def calc_heuristica(matriz, objetivo):
     grafo = {}
@@ -25,28 +22,3 @@ def calc_heuristica(matriz, objetivo):
                 grafo[nodo] = h_manhattan
     return grafo
 
-
-# print(calc_heuristica(laberinto, 4.4))
-# print(map_grafo(laberinto))
-
-# ff = {
-#     0.0: 8,
-#     1.0: 7,
-#     2.0: 6,
-#     3.0: 5,
-#     4.0: 4,
-#     1.1: 6,
-#     4.1: 3,
-#     0.2: 6,
-#     1.2: 5,
-#     2.2: 4,
-#     3.2: 3,
-#     4.2: 2,
-#     0.3: 5,
-#     3.3: 2,
-#     0.4: 4,
-#     1.4: 3,
-#     2.4: 2,
-#     3.4: 1,
-#     4.4: 0,
-# }
