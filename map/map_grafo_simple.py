e48ef334aada54a148584cc1d7d5cabd35f3297a
palcojulian <palcojulian@outlook.com>
2026-05-14 22:25:05 -0500

    docs: :memo: Agrega documentacion codigo

--- map/map_grafo_simple.py
+++ map/map_grafo_simple.py
@@ -1,12 +1,15 @@
-# laberinto = [
-#     [1, 0, 1, 1, 1],
-#     [1, 1, 1, 0, 1],
-#     [1, 0, 1, 0, 1],
-#     [1, 0, 1, 1, 1],
-#     [1, 1, 1, 0, 1],
-# ]
+"""
+Funcion encargada de generar el grafo a partir de una matriz, -
+en donde la key es el nodo 'i,j' y el value son los vecinos a donde 
+se puede mover, se analiza cada posicion.
 
-
+Grafo de ejemplo
+{
+    '0.0': ['1.0'],
+    '1.0': ['1.1', '0.0', '2.0'],
+    ...
+}
+"""
 def map_grafo(matriz):
     grafo = {}
     filas = len(matriz)
@@ -32,29 +35,3 @@ def map_grafo(matriz):
 
                 grafo[nodo] = vecinos
     return grafo
-
-
-# print(map_grafo(laberinto))
-
-
-# test = {
-#     0.0: [1.0],
-#     1.0: [1.1, 0.0, 2.0],
-#     2.0: [1.0, 3.0],
-#     3.0: [2.0, 4.0],
-#     4.0: [4.1, 3.0],
-#     1.1: [(1.0, 6), 1.2],
-#     4.1: [(4.0, 6), 4.2],
-#     0.2: [0.3, 1.2],
-#     1.2: [(1.1, 6), 0.2, 2.2],
-#     2.2: [1.2, 3.2],
-#     3.2: [3.3, 2.2, 4.2],
-#     4.2: [(4.1, 6), 3.2],
-#     0.3: [(0.2, 6), 0.4],
-#     3.3: [(3.2, 6), 3.4],
-#     0.4: [(0.3, 6), 1.4],
-#     1.4: [0.4, 2.4],
-#     2.4: [1.4, 3.4],
-#     3.4: [(3.3, 6), 2.4, 4.4],
-#     4.4: [3.4],
-# }
